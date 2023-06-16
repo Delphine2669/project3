316c5ad10537eb8556a99629ac63f781e990e02e
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-06-16 11:50:04 +0200

    modified video file added track caption

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,14 +1,19 @@
-import Video1 from "../../../backend/public/assets/videos/Cyberpucnk_2077_court_circuit.mp4";
-import Video2 from "../../../backend/public/assets/videos/Cyberpucnk_2077_Panam_in_trouble.mp4";
+import Video1 from "/Cyberpucnk_2077_court_circuit.mp4";
+import Video2 from "/Cyberpucnk_2077_Panam_in_trouble.mp4";
 
 function Videos() {
+  const caption1 = "/video1.vtt";
+  const caption2 = "/video2.vtt";
+
   return (
     <div>
       <video controls src={Video1} width="300px" height="150px">
         Your browser does not support the video tag.
+        <track default kind="captions" srcLang="fr" src={caption1} />
       </video>
       <video controls src={Video2} width="300px" height="150px">
         Your browser does not support the video tag.
+        <track default kind="captions" srcLang="fr" src={caption2} />
       </video>
     </div>
   );
