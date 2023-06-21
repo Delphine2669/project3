31b68b60c00a3eb6a9d3e6366ea148ae7ee0cc5f
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-06-21 10:53:00 +0200

    changed path

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -2,8 +2,8 @@ function Video() {
   const caption1 = "/video1.vtt";
   const caption2 = "/video2.vtt";
 
-  const Video1 = "/Cyberpucnk_2077_court_circuit.mp4";
-  const Video2 = "/Cyberpucnk_2077_Panam_in_trouble.mp4";
+  const Video1 = "/assets/Cyberpucnk_2077_court_circuit.mp4";
+  const Video2 = "/assets/Cyberpucnk_2077_Panam_in_trouble.mp4";
   return (
     <div>
       <video controls src={Video1} width="300px" height="150px">
