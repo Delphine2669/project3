42273a45fb98656c807965ceab89a8b9734dd8c6
Taupe L'Humide <nicolasrabrioux.nr@gmail.com>
2023-06-21 15:11:50 +0200

    arrange css component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../../components/Navbar/Navbar";
+import NavBar from "../../components/NavBar/Navbar";
 import Footer from "../../components/Footer";
 import "../../components/Footer.scss";
 
