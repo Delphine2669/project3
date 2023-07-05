65656333606198e9a3e8579419dde09e81ccb757
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-07-05 19:50:14 +0200

    route updated

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const videoControllers = require("./controllers/videoControllers");
+const viewerControllers = require("./controllers/viewerControllers");
 
 router.get("/videos", videoControllers.browse);
 router.get("/videos/:id", videoControllers.read);
 router.put("/videos/:id", videoControllers.edit);
 router.post("/videos", videoControllers.add);
 router.delete("/videos/:id", videoControllers.destroy);
+router.get("/viewer", viewerControllers.browse);
+router.get("/videos/:id", viewerControllers.read);
+router.put("/videos/:id", viewerControllers.edit);
+router.post("/videos", viewerControllers.add);
+router.delete("/videos/:id", viewerControllers.destroy);
 
+router.post("/videos/login");
 module.exports = router;
