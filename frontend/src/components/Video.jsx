f6a05dcd2c70119730880c531c866ed5e560d144
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-07-05 17:02:25 +0200

    wip

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,14 +1,8 @@
 import PropTypes from "prop-types";
 import VideoCard from "./Caroussel/VideoCard";
-import "./Video.css";
+import "./Video.scss";
 
 function Video({ videoList }) {
-  // const videos = [
-  //   "Cyberpucnk_2077_court_circuit.mp4",
-  //   "Cyberpucnk_2077_Panam_in_trouble.mp4",
-  //   "Cyberpucnk_2077_court_circuit.mp4",
-  //   "Cyberpucnk_2077_Panam_in_trouble.mp4",
-  // ];
   return (
     <div className="video-carousel">
       <p className="catégories"> Catégorie RPG</p>
