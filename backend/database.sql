2cd725f2c4de10b6865cf8b06794060eaaaf81bc
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-07-11 22:06:58 +0200

    change db

--- backend/database.sql
+++ backend/database.sql
@@ -66,16 +66,12 @@ CREATE TABLE `video` (
 
 LOCK TABLES `video` WRITE;
 /*!40000 ALTER TABLE `video` DISABLE KEYS */;
-<<<<<<< HEAD
-INSERT INTO `video` VALUES (3,'Cyberpucnk_2077_court_circuit',7,'premier_test','2023-06-14',0,1,'/videos/Cyberpucnk_2077_court_circuit.mp4',NULL),(4,'Cyberpucnk_2077_Panam_in_trouble',11,'second_test','2023-06-15',0,1,'/videos/Cyberpucnk_2077_Panam_in_trouble.mp4',NULL);
-=======
 INSERT INTO `video` VALUES (3,'Cyberpunck_2077_court_circuit',7,'premier_test','2023-06-14',0,1,'/videos/Cyberpunck_2077_court_circuit.mp4'),
 (4,'Cyberpunck_2077_Panam_in_trouble',11,'second_test','2023-06-15',0,1,'/videos/Cyberpunck_2077_Panam_in_trouble.mp4'),
 (5,'Cacahuette',9,'call Of video 1','2023-07-10',0,1,'/videos/video1.mp4'),
 (6,'Cacahuette 2 le retour',10,'call Of video 2','2023-07-10',0,1,'/videos/video2.mp4'),
 (7,'Cacahuette 3 la suite de trop ?',10,'call Of video 3','2023-07-10',0,1,'/videos/video3.mp4'),
 (8,"Cacahuette 4, trop c'est trop",10,'call Of video 4','2023-07-10',0,1,'/videos/video4.mp4');
->>>>>>> dev
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
 
