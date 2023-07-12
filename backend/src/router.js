9d2da887f946957080741c96cf9937873b050cc7
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-07-12 20:46:13 +0200

    upload backend

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
 
+const upload = multer({ dest: "./public/uploads" });
+const {
+  hashPassword,
+  verifyPassword,
+  verifyToken,
+} = require("./middlewares/services/auth");
 const videoControllers = require("./controllers/videoControllers");
 
 router.get("/videos/:id", videoControllers.read);
@@ -11,11 +20,18 @@ const viewerControllers = require("./controllers/viewerControllers");
 router.get("/viewer", viewerControllers.browse);
 router.get("/viewer/:id", viewerControllers.read);
 
-const {
-  hashPassword,
-  verifyPassword,
-  verifyToken,
-} = require("./middlewares/services/auth");
+router.post("/viewer/video", upload.single("video"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 router.post("/viewer", hashPassword, viewerControllers.add);
 
