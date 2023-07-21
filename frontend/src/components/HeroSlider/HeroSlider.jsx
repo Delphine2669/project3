e025b6f510da157fdeff30648259fa04771ad3c1
Waheb26 <cherwaheb@gmail.com>
2023-07-21 11:19:08 +0200

    work heroslider

--- frontend/src/components/HeroSlider/HeroSlider.jsx
+++ frontend/src/components/HeroSlider/HeroSlider.jsx
@@ -4,9 +4,18 @@ import "./HeroSlider.scss";
 
 function HeroSlider({ photoList }) {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isImageModalOpen, setIsImageModalOpen] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
   const handleSlideClick = (index) => {
-    setCurrentSlide(index);
+    setIsImageModalOpen(true);
+    setSelectedImageIndex(index);
   };
+
+  const closeImageModal = () => {
+    setIsImageModalOpen(false);
+  };
+
   const goToNextSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === photoList.length - 1 ? 0 : prevSlide + 1
@@ -18,11 +27,14 @@ function HeroSlider({ photoList }) {
       prevSlide === 0 ? photoList.length - 1 : prevSlide - 1
     );
   };
+
   const handleKeyDown = (event) => {
     if (event.key === "ArrowRight") {
       goToNextSlide();
     } else if (event.key === "ArrowLeft") {
       goToPreviousSlide();
+    } else if (event.key === "Escape") {
+      closeImageModal();
     }
   };
 
@@ -53,9 +65,29 @@ function HeroSlider({ photoList }) {
           </div>
         ))}
       </div>
+
+      {isImageModalOpen && (
+        <div className="image-modal">
+          <div className="image-modal-content">
+            <img
+              src={photoList[selectedImageIndex].src}
+              alt={photoList[selectedImageIndex].alt}
+            />
+            <button
+              type="button"
+              className="modal-close-button"
+              onClick={closeImageModal}
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
+
 HeroSlider.propTypes = {
   photoList: PropTypes.arrayOf(
     PropTypes.shape({
