e6ccd4bb18e5dcdfd40bec03f555f44a9506c647
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-07-26 09:41:06 +0200

    fix err 404 font clinton

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,22 +1,19 @@
 import { NavLink } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
+// import { useAuth } from "../../contexts/AuthContext";
 import "./Navbar.scss";
 import ProfileMenu from "../ProfileMenu/ProfileMenu";
 import logo from "../../assets/logo.png";
 // import Searchbar from "../SearchBar/SearchBar";
 
 function NavBar() {
-  const { cachedToken, isAdmin } = useAuth();
   return (
     <div className="nav-container">
       <NavLink to="/">
         <img className="logo" src={logo} alt="Logo TSN Game" />
       </NavLink>
-      {cachedToken && isAdmin && (
-        <div className="admin-dash-link">
-          <NavLink to={isAdmin ? "/adminpage" : "/"}>Admin Dashboard</NavLink>
-        </div>
-      )}
+      <div className="admin-dash-link">
+        <NavLink to="/adminpage">Admin Dashboard</NavLink>
+      </div>
       <div className="link">
         {/* <Searchbar /> */}
         <ProfileMenu />
