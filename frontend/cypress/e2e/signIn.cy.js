28c3a43501bd7814ec0ad0be529b75e0578ec03e
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-12-11 16:34:44 +0100

    added new test in e2e

--- frontend/cypress/e2e/signIn.cy.js
+++ frontend/cypress/e2e/signIn.cy.js
@@ -3,20 +3,25 @@ describe("testing signup, login and heroslider", () => {
     cy.visit("http://localhost:3000/signup");
     cy.get(":nth-child(2) > .signup-input").type("dd26om");
     cy.get(":nth-child(3) > .signup-input").type("dd26om@tsn.game");
-    cy.get(":nth-child(4) > .signup-input").type("1234");
-    cy.get(":nth-child(5) > .signup-input").type("1234");
+    cy.get(":nth-child(4) > .signup-input").type("!Wild26!");
+    cy.get(":nth-child(5) > .signup-input").type("!Wild26!");
     cy.get(".signup-submit").click();
   });
 
   it("fills the login form and connects", () => {
     cy.visit("http://localhost:3000/login");
     cy.get("#username").type("dd26om");
-    cy.get("#password").type("1234");
+    cy.get("#password").type("!Wild26!");
     cy.get(".button-text").click();
   });
   it("tests that the slider changes when buttons left or right are clicked", () => {
     cy.visit("http://localhost:3000/");
     cy.get(".control-next").click({ force: true }).click({ force: true });
     cy.get(".control-prev").click({ force: true });
+  });
+  it("tests the video modal", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[src="/assets/logo/csgologo.png"]').click({ force: true });
+    cy.get(".video-modal-close-button > span").click({ force: true });
   });
 });
