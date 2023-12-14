c1766789e64e717865742a8d23c0945dd60b8f11
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-12-14 16:57:39 +0100

    changed e2e

--- frontend/cypress/e2e/signIn.cy.js
+++ frontend/cypress/e2e/signIn.cy.js
@@ -1,18 +1,24 @@
-describe("testing signup, login and heroslider", () => {
+describe("testing signup, login and heroslider and videoModal", () => {
+  beforeEach(() => {
+    cy.viewport(390, 844);
+  });
   it("fills the signup form", () => {
     cy.visit("http://localhost:3000/signup");
-    cy.get(":nth-child(2) > .signup-input").type("dd26om");
-    cy.get(":nth-child(3) > .signup-input").type("dd26om@tsn.game");
-    cy.get(":nth-child(4) > .signup-input").type("!Wild26!");
-    cy.get(":nth-child(5) > .signup-input").type("!Wild26!");
+    cy.get(":nth-child(2) > .signup-input").type("user12");
+    cy.get(":nth-child(3) > .signup-input").type("user12@gmail.com");
+    cy.get(":nth-child(4) > .signup-input").type("SuperMario12*");
+    cy.get(":nth-child(4) > .toggle-password > img").click({ force: true });
+    cy.get(":nth-child(5) > .signup-input").type("SuperMario12*");
     cy.get(".signup-submit").click();
+    cy.screenshot("signup form");
   });
 
   it("fills the login form and connects", () => {
     cy.visit("http://localhost:3000/login");
-    cy.get("#username").type("dd26om");
-    cy.get("#password").type("!Wild26!");
+    cy.get("#username").type("user12");
+    cy.get("#password").type("SuperMario12*");
     cy.get(".button-text").click();
+    cy.screenshot("homepage");
   });
   it("tests that the slider changes when buttons left or right are clicked", () => {
     cy.visit("http://localhost:3000/");
@@ -21,7 +27,7 @@ describe("testing signup, login and heroslider", () => {
   });
   it("tests the video modal", () => {
     cy.visit("http://localhost:3000/");
-    cy.get('[src="/assets/logo/csgologo.png"]').click({ force: true });
+    cy.get('[src="/assets/logo/csgologo.webp"]').click({ force: true });
     cy.get(".video-modal-close-button > span").click({ force: true });
   });
 });
